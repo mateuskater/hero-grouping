00471d8e47cf3f6959675fe12e58c370eea44849
MATEUS KATER POMBEIRO <mkp19@inf.ufpr.br>
2023-06-21 17:26:19 -0300

    the beginning

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct heroi{
+    int index;
+    int conf;
+    int afin;
+}heroi;
+
+int main(){
+    int itens, conflitos, afinidades;
+    scanf("%d %d %d", &itens, &conflitos, &afinidades);
+
+    for (int i = 0; i < conflitos + afinidades; i++){
+        
+    }
+}
